b10718086bec65bf592718e8dde0665ec15298e1
hurdong <gjehdals456@gmail.com>
2025-06-27 15:20:22 +0900

    [Init] 백엔드 프로젝트 초기 설정 및 파일 추가: .gitignore, build.gradle, gradlew, gradlew.bat, settings.gradle, 애플리케이션 메인 클래스, 데이터베이스 설정을 포함한 application.yml, 테스트 클래스 추가

---
+++ BE/src/main/java/com/stockr/be/StockrBeApplication.java
@@ -0,0 +1,13 @@
+package com.stockr.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockrBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockrBeApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/stockr/be/StockrBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockr.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockrBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
