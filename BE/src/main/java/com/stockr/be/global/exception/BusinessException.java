271457c4204391ca472a2b2c164aa8c081d69044
HurDong <gjehdals456@gmail.com>
2025-06-30 02:53:02 +0900

    [refactor] BusinessException 클래스에서 ErrorCode 열거형 제거: 불필요한 코드 정리

--- BE/src/main/java/com/stockr/be/global/exception/BusinessException.java
+++ BE/src/main/java/com/stockr/be/global/exception/BusinessException.java
@@ -16,15 +16,3 @@ public class BusinessException extends RuntimeException {
         this.errorCode = errorCode;
     }
 }
-
-@Getter
-public enum ErrorCode {
-    STOCK_PRICE_NOT_FOUND("주식 가격 정보를 찾을 수 없습니다."),
-    INVALID_STOCK_PRICE("잘못된 주식 가격 정보입니다.");
-
-    private final String message;
-
-    ErrorCode(String message) {
-        this.message = message;
-    }
-}
