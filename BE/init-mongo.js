cdc563f60bd302c68b8ad8d61c716abd6717d718
kopo <2560340086@office.kopo.ac.kr>
2025-06-30 12:05:53 +0900

    [BE] fix: MongoDB 사용자 생성 시 오류 처리 추가

--- BE/init-mongo.js
+++ BE/init-mongo.js
@@ -1,10 +1,19 @@
 db = db.getSiblingDB("admin");
 
-db.createUser({
-  user: process.env.MONGO_INITDB_ROOT_USERNAME,
-  pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
-  roles: [{ role: "root", db: "admin" }],
-});
+try {
+  db.createUser({
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+    roles: [{ role: "root", db: "admin" }],
+  });
+  print("User created successfully");
+} catch (error) {
+  if (error.code === 51003) {
+    print("User already exists, skipping creation");
+  } else {
+    print("Error creating user: " + error.message);
+  }
+}
 
 db = db.getSiblingDB(process.env.MONGO_DATABASE);
 
