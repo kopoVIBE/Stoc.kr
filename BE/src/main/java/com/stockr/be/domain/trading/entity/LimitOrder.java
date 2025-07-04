b5751083bd1afb1b9c5dc3e5f23cec49af061104
HurDong <gjehdals456@gmail.com>
2025-07-05 05:35:46 +0900

    [FEAT] 거래 로그 엔티티 수정 및 주문 처리 로직 개선: LimitOrder에 Builder 추가, TradeLog에서 외래 키 제거 및 주문 ID 저장 방식 변경, InternalTradeService에서 거래 로그 생성 시 주문 ID 사용

--- BE/src/main/java/com/stockr/be/domain/trading/entity/LimitOrder.java
+++ BE/src/main/java/com/stockr/be/domain/trading/entity/LimitOrder.java
@@ -15,11 +15,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LimitOrder {
