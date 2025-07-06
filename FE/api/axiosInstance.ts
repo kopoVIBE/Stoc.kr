7df56aaf6c3768406c56a5abbeba43736e6525ad
HurDong <happycodingmin@gmail.com>
2025-07-06 22:20:42 +0900

    [FIX] axiosInstance의 baseURL을 로컬 주소에서 서버 주소로 변경

--- FE/api/axiosInstance.ts
+++ FE/api/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080", // 백엔드 주소
+  baseURL: "http://13.125.23.79:8080", // 백엔드 주소
   headers: {
     "Content-Type": "application/json",
   },
